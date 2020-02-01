7a5af08a20ac05e93583dce87c61e04cb0c23550
Darina Kavokina <kuz-dana@mail.ru>
2020-02-01 02:22:44 +0100

    Initialized dice class, added enum for event dice

---
+++ Colonizers/Dice/Dice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colonizers.Dice
+{
+	class Dice
+	{
+		#region Fields
+
+		int topSide;
+		int numSides = 6;
+
+		#endregion
+
+		#region Properties
+
+		public int TopSide
+		{
+			get { return topSide; }
+		}
+
+		public int NumSides
+		{
+			get { return numSides; }
+		}
+
+		#endregion
+
+		#region Constructors
+
+		public Dice(int numSides)
+		{
+			this.numSides = numSides;
+			topSide = 1;
+		}
+
+		#endregion
+
+		#region Methods
+
+		public void Roll()
+		{
+			//Create Random number generator
+			//topSide = RandomNumberGenerator.Next(numSides) + 1;
+		}
+
+		#endregion
+	}
+}
